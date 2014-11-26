48cc77c4a2dc327734d8a0970234132922109ecd
Henning Kvinnesland <henning@keyteq.no>
2014-11-26 11:51:44 +0100

    Implemented support for JSON.

--- src/Flageolett/eZCompletionBundle/Command/CompletionCommand.php
+++ src/Flageolett/eZCompletionBundle/Command/CompletionCommand.php
@@ -6,10 +6,8 @@
 
 namespace Flageolett\eZCompletionBundle\Command;
 
-use eZ\Publish\API\Repository\Values\ContentType\ContentTypeGroup;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CompletionCommand extends ContainerAwareCommand
@@ -19,84 +17,15 @@ class CompletionCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this->setName('ezcode:completion');
-        $this->addOption(self::OPTION_NAME_COMPLETION_TYPE, 't', InputOption::VALUE_REQUIRED);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $type = $input->getOption(self::OPTION_NAME_COMPLETION_TYPE);
-        switch ($type) {
-            case 'contentclass':
-                $result = $this->contentClass();
-            break;
-            case 'contentclassid':
-                $result = $this->contentClassId();
-            break;
-            case 'contentclassgroup':
-                $result = $this->contentClassGroup();
-                break;
-            case 'contentclassgroupid':
-                $result = $this->contentClassGroupId();
-                break;
-            default:
-                $result = '';
-        }
+        $container = $this->getContainer();
 
-        $output->writeln($result);
-    }
+        $completions = array();
+        $completions += $container->get('ezcompletionbundle.contenttype')->getCompletions();
 
-    protected function contentTypeService()
-    {
-        return $this->getContainer()->get('ezpublish.api.repository')->getContentTypeService();
-    }
-
-    protected function getContentClassGroups()
-    {
-        return $this->contentTypeService()->loadContentTypeGroups();
-    }
-
-    protected function contentClassGroup()
-    {
-        $groups = $this->getContentClassGroups();
-        return array_map(function(ContentTypeGroup $group)
-        {
-            return $group->identifier;
-        }, $groups);
-    }
-
-    protected function contentClass()
-    {
-        $groups = $this->getContentClassGroups();
-        $classList = array();
-        foreach ($groups as $group) {
-            $contentTypes = $this->contentTypeService()->loadContentTypes($group);
-            foreach ($contentTypes as $contentType) {
-                $classList[] = $contentType->identifier;
-            }
-        }
-
-        return $classList;
-    }
-
-    protected function contentClassGroupId()
-    {
-        $groups = $this->getContentClassGroups();
-        return array_map(function (ContentTypeGroup $group) {
-            return $group->id . ';' . $group->identifier;
-        }, $groups);
-    }
-
-    protected function contentClassId()
-    {
-        $groups = $this->getContentClassGroups();
-        $classList = array();
-        foreach ($groups as $group) {
-            $contentTypes = $this->contentTypeService()->loadContentTypes($group);
-            foreach ($contentTypes as $contentType) {
-                $classList[] = $contentType->id . ';' . $contentType->identifier;
-            }
-        }
-
-        return $classList;
+        $output->writeln(json_encode($completions));
     }
 }
