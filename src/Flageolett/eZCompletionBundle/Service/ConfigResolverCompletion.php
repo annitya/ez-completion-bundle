f2e34af758f90eae4133c60cfdcad5b4a20946b5
Henning Kvinnesland <henning@keyteq.no>
2015-03-14 23:17:14 +0100

    Added completions for $repository->canUser(...)

--- src/Flageolett/eZCompletionBundle/Service/ConfigResolverCompletion.php
+++ src/Flageolett/eZCompletionBundle/Service/ConfigResolverCompletion.php
@@ -15,12 +15,12 @@ class ConfigResolverCompletion extends CompletionAbstract
     /** @var ContainerInterface */
     protected $container;
     /** @var string */
-    protected $defaultNamespace;
+    protected $namespacePart;
 
     public function __construct(ContainerInterface $container, $defaultNamespace)
     {
         $this->container = $container;
-        $this->defaultNamespace = $defaultNamespace;
+        $this->namespacePart = $defaultNamespace . '.';
     }
 
     protected function getDataSource()
@@ -30,25 +30,15 @@ class ConfigResolverCompletion extends CompletionAbstract
         $property->setAccessible(true);
 
         $parameters = array_keys($property->getValue($this->container));
-        $siteaccesses = $this->container->getParameter('ezpublish.siteaccess.groups_by_siteaccess');
-        $settingGroups = array();
-        foreach ($siteaccesses as $siteaccess => $groupList) {
-            $settingGroups[] = $siteaccess;
-            $settingGroups = array_merge($settingGroups, $groupList);
-        }
-        $settingGroups[] = ConfigResolver::SCOPE_DEFAULT;
-        $settingGroups[] = ConfigResolver::SCOPE_GLOBAL;
-        $settingGroups = array_unique($settingGroups);
-
-        $namespacePart = $this->defaultNamespace . '.';
+        $settingGroups = $this->getSettingGroups();
 
         $eZParameters = array();
         foreach ($parameters as $parameter) {
             // Lets skip the standard parameters.
-            if (strpos($parameter, $namespacePart) !== 0) {
+            if (strpos($parameter, $this->namespacePart) !== 0) {
                 continue;
             }
-            $parameter = str_replace($namespacePart, '', $parameter);
+            $parameter = str_replace($this->namespacePart, '', $parameter);
             foreach ($settingGroups as $group) {
                 $groupPart = $group . '.';
                 if (strpos($parameter, $groupPart) === 0) {
@@ -61,5 +51,22 @@ class ConfigResolverCompletion extends CompletionAbstract
         }
 
         return array('parameters' => array_values($eZParameters));
+    }
+
+    /**
+     * @return array
+     */
+    protected function getSettingGroups()
+    {
+        $siteaccesses = $this->container->getParameter('ezpublish.siteaccess.groups_by_siteaccess');
+        $settingGroups = array();
+        foreach ($siteaccesses as $siteaccess => $groupList) {
+            $settingGroups[] = $siteaccess;
+            $settingGroups = array_merge($settingGroups, $groupList);
+        }
+        $settingGroups[] = ConfigResolver::SCOPE_DEFAULT;
+        $settingGroups[] = ConfigResolver::SCOPE_GLOBAL;
+
+        return array_unique($settingGroups);
     }
 }
